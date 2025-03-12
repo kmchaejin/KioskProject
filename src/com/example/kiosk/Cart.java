a918cfb7a082cb51f9868c7674fcbccd8d58d598
t2023-m0082 <t2023-m0082@t2023-m0082ui-MacBookAir.local>
2025-03-12 10:47:17 +0900

    feat: 장바구니 클래스 생성(#12)

---
+++ src/com/example/kiosk/Cart.java
@@ -0,0 +1,22 @@
+package com.example.kiosk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private String name;
+    private double price;
+    private int quantity;
+    private double totalPrice;
+
+    private List<MenuItem> cartItemList = new ArrayList<>();
+
+    public void addCartItem(MenuItem item){
+        cartItemList.add(item);
+    }
+
+    public List<MenuItem> getCartItemList(){
+        return cartItemList;
+    }
+
+}
